ab3929f2130f72c68f219b1ca9451e68c0d4a34b
AardvarksUpInYoFace <jonathanjacob_L@hotmail.com>
2019-01-26 11:50:07 +0000

    crab movement

---
+++ Assets/Resources/Crab Control/Crab Scripts/CameraController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject FollowObject;
+
+    private Vector3 StartDifference;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(FollowObject != null)
+        {
+            StartDifference = transform.position - FollowObject.transform.position;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        UpdateCameraPosition();
+    }
+
+    private void UpdateCameraPosition()
+    {
+        Vector3 newObjPos = FollowObject.transform.position;
+        Vector3 newCamPos = newObjPos + StartDifference;
+
+        Vector3 curCamPos = transform.position;
+
+        float newCamLerpPosX = Mathf.Lerp(curCamPos.x, newCamPos.x, 0.1f);
+        float newCamLerpPosY = Mathf.Lerp(curCamPos.y, newCamPos.y, 0.1f);
+        float newCamLerpPosZ = Mathf.Lerp(curCamPos.z, newCamPos.z, 0.1f);
+
+        transform.position = new Vector3(newCamLerpPosX, newCamLerpPosY, newCamLerpPosZ);
+    }
+}
---
+++ Assets/Resources/Crab Control/Crab Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+
+    public float CrabSpeed = 1f;
+
+    private Rigidbody Rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Movement();
+    }
+
+
+
+    private void Movement()
+    {
+        //checking forwards and backwards.
+        int forward = 0;
+        int side = 0;
+
+        forward += CheckInputHeld(new KeyCode[] { KeyCode.W, KeyCode.UpArrow });
+        forward -= CheckInputHeld(new KeyCode[] { KeyCode.S, KeyCode.DownArrow });
+
+        side += CheckInputHeld(new KeyCode[] { KeyCode.D, KeyCode.RightArrow });
+        side -= CheckInputHeld(new KeyCode[] { KeyCode.A, KeyCode.LeftArrow });
+
+        float diagonalOperator = 1;
+        if (Mathf.Abs(forward) + Mathf.Abs(side) > 1) diagonalOperator = 0.707f;
+
+        //GetComponent<Rigid.localPosition += new Vector3(CrabSpeed * diagonalOperator * side, 0, CrabSpeed * diagonalOperator * forward);
+
+        Rb.velocity = new Vector3(CrabSpeed * diagonalOperator * side, 0, CrabSpeed * diagonalOperator * forward);
+    }
+
+    private int CheckInputHeld(KeyCode[] keycodes)
+    {
+        foreach(KeyCode code in keycodes)
+        {
+            if(Input.GetKey(code))
+            {
+                return 1;
+            }
+        }
+
+        return 0;
+    }
+}
